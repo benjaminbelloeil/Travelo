399fb2bc42f36051e2744c2576fdd06b09e12ed4
Dawar Hasnain <46447522+Dawar-hasnain@users.noreply.github.com>
2025-10-10 12:59:45 +0200

    Project Initiate

---
+++ Travelo/Travelo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Travelo
+//
+//  Created by Dawar Hasnain on 10/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Travelo/Travelo/TraveloApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraveloApp.swift
+//  Travelo
+//
+//  Created by Dawar Hasnain on 10/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TraveloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
