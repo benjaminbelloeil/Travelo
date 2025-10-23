913fabbc41b36aebd0d8642cc2c207319fb58787
Benjamin Belloeil <benjaminbelloeil@outlook.com>
2025-10-23 16:45:17 +0200

    feat: guide feature to know the steps of the country that was selected.

---
+++ Travelo/Travelo/Components/StepStateManager.swift
@@ -0,0 +1,8 @@
+//
+//  StepStateManager.swift
+//  Travelo
+//
+//  Created by Benjamin Belloeil on 23/10/25.
+//
+
+import Foundation
---
+++ Travelo/Travelo/Views/StepDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  StepDetailView.swift
+//  Travelo
+//
+//  Created by Benjamin Belloeil on 23/10/25.
+//
+
+import Foundation
